1a4c7090e72e210aaa837503b6d300314abd566b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:49 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -3,26 +3,26 @@ import equals from "@unction/equals";
 
 import range from "./index";
 
-test(() => {
+test("works", () => {
   expect(range(0)(0)).toBe(0);
 });
 
-test(() => {
+test("works", () => {
   expect(range(1)(1)).toBe(1);
 });
 
-test(() => {
+test("works", () => {
   const value = range(0)(1);
 
   expect(equals(0)(value) || equals(1)(value)).toBeTruthy();
 });
 
 
-test(() => {
+test("works", () => {
   expect(range(0)(100)).not.toBe(101);
 });
 
 
-test(() => {
+test("works", () => {
   expect(() => range(1)(0)).toThrow();
 });
